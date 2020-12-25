8ef3e046bca2cf589ae80ee87f5e3765e4ea8bba
Tom Schwarz <tomiam8@outlook.com>
2020-12-25 14:12:53 +1100

    add getting playlist item, depaginating

--- shuffler-prototype.py
+++ shuffler-prototype.py
@@ -16,22 +16,38 @@ def spotify_authenticate(client_id : str):
         raise Exception("Received error response from Spotify during authorization: error={}".format(response['error']))
     return response['access_token']
 
-def get_playlists(client_id : str, access_token : str):
+def get_auth_header(access_token : str):
+    return {"Authorization": "Bearer {}".format(access_token)}
+
+def get_playlists(access_token : str):
     """
     Calls Spotify's Get a List of Current User's playlists - https://api.spotify.com/v1/users/{user_id}/playlists
     Handles the pagination stuff too.
     """
     url = "https://api.spotify.com/v1/me/playlists?limit={}&offset={}"
-    headers = {"Authorization": "Bearer {}".format(access_token)}
-    response = requests.get(url.format(50,0), headers=headers).json()
-    return [{'id':playlist['id'], 'name': playlist['name']} for playlist in response['items']] 
+    response = de_paginator(access_token, requests.get(url.format(50,0), headers=get_auth_header(access_token)).json())
+    print(response)
+    return [{'id':playlist['id'], 'name': playlist['name']} for playlist in response] 
+
+def get_playlist_items(access_token : str, playlist_id : str):
+    url = "https://api.spotify.com/v1/playlists/{}/tracks?market=from_token&fields=items(track(uri))%2Cnext".format(playlist_id)
+    response = de_paginator(access_token, requests.get(url, headers=get_auth_header(access_token)).json())
+    print(response)
+
+def de_paginator(access_token : str, response):
+    items = response['items']
+    while response['next'] is not None:
+        response = requests.get(response['next'], headers=get_auth_header(access_token)).json()
+        items += response['items']
+    return items
 
 
 def main():
     access_token = spotify_authenticate(client_id)
-    playlists = get_playlists(client_id, access_token)
+    playlists = get_playlists(access_token)
 
     print("\n".join(["{}. {}".format(playlist[0], playlist[1]['name']) for playlist in enumerate(playlists)]))
     playlist = playlists[int(input("Enter the number of the playlist you would like to shuffle (just the number! No full stop.): "))]
+    items = get_playlist_items(access_token, playlist['id'])
 
 main()
