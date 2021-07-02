e3201a7dbfb5b7f34d10d8c783e2e20c6e8960f3
Tom Schwarz <tomiam8@outlook.com>
2021-07-02 19:52:13 +1000

    Perry the Platypus! Behold, the DePaginatorgit add -u!

--- shuffler.js
+++ shuffler.js
@@ -1,24 +1,45 @@
 function getPlaylists() {
-    accessToken = document.getElementById('spotifyKey').value;
-    fetch('https://api.spotify.com/v1/me/playlists?limit=50&offset=0', {
-        headers: { "Authorization": "Bearer " + accessToken }
-    })
-    .then(response => response.json())
+    depaginator('https://api.spotify.com/v1/me/playlists?limit=50&offset=0')
     .then(data => {
-        console.log('Success:', data);
         listPlaylists(data);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
     });
 }
 
-function listPlaylists(playlistData) {
+function listPlaylists(playlists) {
     list = document.getElementById("playlistList");
-    console.log(playlistData.items.length);
-    playlistData.items.forEach(function (item) {
-        let li = document.createElement('li');
+    playlists.forEach(function (item) {
+        li = document.createElement('li');
+        but = document.createElement('input');
+        but.type = 'button';
+        but.value = item.name;
+        but.setAttribute("onclick", "shuffle(\"" + item.id + "\");") //probably (definitely) a better way then hardcoding the IDs into the function call lol
+        li.appendChild(but);
         list.appendChild(li);
-        li.innerHTML += item.name;
     });
+}
+
+function shuffle(playlistID) {
+    depaginator('https://api.spotify.com/v1/playlists/' + playlistID + '/tracks?market=from_token&fields=items(track(uri))%2Cnext')
+    .then(data => {
+        console.log(data);
+    });
+}
+
+async function depaginator(URL) {
+    response = await (await fetch(URL, getSpotifyHeaders())).json();
+    items = response.items;
+    while (response.next !== null) {
+        response = await (await fetch(response.next, getSpotifyHeaders())).json();
+        items = items.concat(response.items);
+    }
+    return items;
+}
+
+
+function getSpotifyToken() {
+    return document.getElementById('spotifyKey').value;
+}
+
+function getSpotifyHeaders() {
+    return {headers: {"Authorization": "Bearer " + getSpotifyToken()}}
 }
